851ab495aa05720c03ccac1904fe99fb55062450
Alicen Bender <alicene.bender@gmail.com>
2023-12-30 16:31:16 -0500

    Added HTML, CSS, and JS. Completed the HTML and CSS. Began pseudocode on JS.

---
+++ script.js
@@ -0,0 +1,21 @@
+// Global Variables
+var apiKey = 'b67c7c3b23f4622d3fc413765b9c99ec';
+var searches = [];
+
+// When a user searches a city
+    // It is stored underneath the search bar
+    // The current forecast is displayed with
+        // date
+        // city name
+        // humidiity
+        // temperature
+        // wind speed
+        // current weather icon
+    // The 5 day forecast is displayed with
+        // city name
+        // date
+        // humidiity
+        // temperature
+        // wind speed
+        // current weather icon
+
