b07a32a89e655fef8e89f88c229964873d54a49e
Alicen Bender <alicene.bender@gmail.com>
2023-12-31 11:27:02 -0500

    Created the saved search history for cities.

--- script.js
+++ script.js
@@ -4,6 +4,32 @@ var searches = [];
 
 // When a user searches a city
     // It is stored underneath the search bar
+var cityHistoryList = function(cityName) {
+    $('.past-search:contains("' + cityName + '")').remove();
+
+    var cityHistoryEntry = $("<p>");
+    cityHistoryEntry.addClass("past-search");
+    cityHistoryEntry.text(cityName);
+
+    var cityEntryContainer = $("<div>");
+    cityEntryContainer.addClass("past-search-container");
+
+    cityEntryContainer.append(cityHistoryEntry);
+
+    var cityHistoryContainerEl = $("#search-history-container");
+    cityHistoryContainerEl.append(cityEntryContainer);
+
+    if (savedSearches.length > 0){
+        var previousSavedSearches = localStorage.getItem("savedSearches");
+        savedSearches = JSON.parse(previousSavedSearches);
+    }
+
+    savedSearches.push(cityName);
+    localStorage.setItem("savedSearches", JSON.stringify(savedSearches));
+
+    $("#search-input").val("");
+
+};
     // The current forecast is displayed with
         // date
         // city name
